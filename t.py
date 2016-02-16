5ea832dfc5e2d056b0d83ec892a5f39cea526726
Shane Dempsey <sdempsey138@gmail.com>
2016-02-16 22:27:00 +0000

    made usable!

--- t.py
+++ t.py
@@ -1,151 +1,159 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import shutil
-import os
-import sys
-import json
 import argparse
+import json
+import os
+import shutil
+import sys
 
-TEMPLATE_DIR = 'templates'
-DESCRIPTION_FILE = '.templates'
-IGNORE_LIST = [DESCRIPTION_FILE, '.DS_Store', '.', '..'] 
-SETTINGS = os.path.join(os.path.expanduser('~'), '.t.py.settings')
-
-def exists(template):
-    template = os.path.join(TEMPLATE_DIR, template)
-    return os.path.exists(template)
+description_file_name = '.templates'
 
 
-def copy_template(template, dst, overwrite=False):
-    if exists(template):
-        src = os.path.join(TEMPLATE_DIR, template)
-        if os.path.exists(dst) and not overwrite:
-            sys.exit(dst + ' already exists')
+class Templates:
+    def __init__(self):
+        # TODO rename "t_dir"
+        t_dir = os.path.join(os.path.expanduser('~'), '.t.py')
+        if not os.path.exists(t_dir):
+            self.initial_setup(t_dir)
+        config_file = os.path.join(t_dir, 'config')
+        config = json.loads(open(config_file).read())
+        self.template_dir = config.get('template_dir')
 
-        if os.path.isdir(src):
-            shutil.copytree(src, dst)
-        else:
-            shutil.copyfile(src, dst)
-    else:
-        sys.exit('Template ' + template + ' does not exist')
+        self.description_file = os.path.join(
+            self.template_dir, description_file_name)
+        self.ignore_list = [description_file_name, '.DS_Store', '.', '..']
 
+    def initial_setup(self, t_dir):
+        template_dir = os.path.join(t_dir, 'templates')
+        os.makedirs(t_dir)
+        os.makedirs(template_dir)
+        config_file = os.path.join(t_dir, 'config')
+        config = {
+            'template_dir': template_dir,
+        }
+        open(config_file, 'w').write(
+            json.dumps(config, sort_keys=True, indent=4) + os.linesep)
 
-def list_templates():
-    if os.path.exists(TEMPLATE_DIR):
-        l = os.listdir(TEMPLATE_DIR)
-        for i in IGNORE_LIST:
-            if i in l:
-                l.remove(i)
-        if len(l) > 0:
-            d = load_descriptions()
-            for t in sorted(l):
-                if t in d:
-                    print(t, '-', d[t])
-                else:
-                    print(t)
-        else:
-            print('There are no templates in ', TEMPLATE_DIR)
-    else:
-        print('Directory', TEMPLATE_DIR, ' does not exist')
-    print('')
+    def copy_template(self, template, dst, overwrite=False):
+        if self.exists(template):
+            src = os.path.join(self.template_dir, template)
+            if os.path.exists(dst) and not overwrite:
+                sys.exit(dst + ' already exists')
 
-
-def remove(template):
-    if exists(template):
-        d = load_descriptions()
-        if template in d:
-            del d[template]
-            open(os.path.join(TEMPLATE_DIR, DESCRIPTION_FILE), 'w').write( json.dumps(d, sort_keys=True, indent=4) + os.linesep )
-        t = os.path.join(TEMPLATE_DIR, template)
-        if os.path.isdir(t):
-            shutil.rmtree(t)
-        else:
-            os.remove(t)
-
-
-def load_descriptions():
-    p = os.path.join(TEMPLATE_DIR, DESCRIPTION_FILE)
-    d = {}
-    if os.path.exists(p):
-        d = json.loads( open(p).read() )
-    return d
-
-
-def update_description(template, description):
-    d = load_descriptions()
-    if exists(template):
-        if len(description) == 0:
-            sys.exit('Please provide a description')
-        else: 
-            d[template] = description
-            open(os.path.join(TEMPLATE_DIR, DESCRIPTION_FILE), 'w').write( json.dumps(d, sort_keys=True, indent=4) + os.linesep )
-    else:
-        sys.exit('Template ' + template + ' does not exist')
-
-
-def add_template(src, template, description, overwrite=False):
-    if os.path.exists(src):
-        destination = os.path.join(TEMPLATE_DIR, template)
-        if os.path.exists(destination) and not overwrite:
-            sys.exit(template + ' already exists')
-        else:
             if os.path.isdir(src):
-                shutil.copytree(src, destination)
+                shutil.copytree(src, dst)
             else:
-                shutil.copyfile(src, destination)
+                shutil.copyfile(src, dst)
+                shutil.copymode(src, dst)
+        else:
+            sys.exit('Template ' + template + ' does not exist.')
 
-            if len(description) > 0:
-                update_description(template, description)
-    else:
-        sys.exit('File ' + src + ' does not exist')
+    def list_templates(self):
+        if os.path.exists(self.template_dir):
+            templates = set(
+                os.listdir(self.template_dir)).difference(set(self.ignore_list))
+            if len(templates) > 0:
+                descriptions = self.load_descriptions()
+                for t in sorted(templates):
+                    if t in descriptions:
+                        print('{:10} - {}'.format(t, descriptions[t]))
+                    else:
+                        print(t)
+            else:
+                print('There are no templates in', self.template_dir)
+        else:
+            print('Directory', self.template_dir, 'does not exist.')
+        print('')
+
+    def delete(self, template):
+        if self.exists(template):
+            d = self.load_descriptions()
+            if template in d:
+                del d[template]
+                open(self.description_file, 'w').write(
+                    json.dumps(d, sort_keys=True, indent=4) + os.linesep)
+            t = os.path.join(self.template_dir, template)
+            if os.path.isdir(t):
+                shutil.rmtree(t)
+            else:
+                os.remove(t)
+            print('Deleted {} from {}.'.format(template, self.template_dir))
+
+    def load_descriptions(self):
+        d = {}
+        if os.path.exists(self.description_file):
+            d = json.loads(open(self.description_file).read())
+        return d
+
+    def update_description(self, template, description):
+        d = self.load_descriptions()
+        if self.exists(template):
+            if len(description) == 0:
+                sys.exit('Please provide a description')
+            else:
+                d[template] = description
+                open(self.description_file, 'w').write(
+                    json.dumps(d, sort_keys=True, indent=4) + os.linesep)
+        else:
+            sys.exit('Template ' + template + ' does not exist.')
+
+    def add_template(self, src, template_name, description, overwrite=False):
+        if os.path.exists(src):
+            destination = os.path.join(self.template_dir, template_name)
+            if os.path.exists(destination) and not overwrite:
+                sys.exit('Error: template {} already exists.'.format(template_name))
+            else:
+                if os.path.isdir(src):
+                    shutil.copytree(src, destination)
+                else:
+                    shutil.copyfile(src, destination)
+
+                if len(description) > 0:
+                    self.update_description(template_name, description)
+                print('Added {} to {}.'.format(template_name, self.template_dir))
+        else:
+            sys.exit('File {} does not exist.'.format(src))
+
+    def exists(self, template):
+        template = os.path.join(self.template_dir, template)
+        return os.path.exists(template)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             description='A script to manage and copy templates of scripts.')
-    parser.add_argument('-a', '--add', help='add a template')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-a', '--add', help='add a template')
     parser.add_argument('-n', '--name', help='name of template')
-    parser.add_argument('-u', '--update', help='update a templates description')
-    parser.add_argument('-R', '--remove', help='template to remove')
-    parser.add_argument('-l', '--list', help='list templates', action='store_true')
-    parser.add_argument('-o', '--overwrite', help='allow overwriting', action='store_true')
-    parser.add_argument('-T', '--template_dir', help='template directory')
+    group.add_argument('-u', '--update', help='update a templates description')
+    group.add_argument('-d', '--delete', help='template to delete')
+    group.add_argument('-l', '--list', help='list templates',
+        action='store_true')
+    parser.add_argument('-o', '--overwrite', help='allow overwriting',
+        action='store_true')
     parser.add_argument('args', nargs='*')
     options = parser.parse_args()
-    
-    if not options.template_dir and os.path.exists(SETTINGS):
-        d = json.loads( open(SETTINGS).read() )
-        TEMPLATE_DIR = d.get('template_dir', TEMPLATE_DIR)
-    elif options.template_dir:
-        TEMPLATE_DIR = options.template_dir
 
-    # create template directory if it doesnt exist
-    if not os.path.exists(TEMPLATE_DIR):
-        os.makedirs(TEMPLATE_DIR)
-    
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
+    t = Templates()
+
     if options.list:
-        list_templates()
+        t.list_templates()
     elif options.add:
         template_name = options.add.split(os.sep)[-1]
         if options.name:
             template_name = options.name
-        add_template( options.add, template_name, ' '.join(options.args),
-                overwrite=options.overwrite )
+        t.add_template(options.add, template_name, ' '.join(options.args),
+                overwrite=options.overwrite)
     elif options.update:
-        update_description( options.update, ' '.join(options.args) )
-    elif options.remove:
-        remove(options.remove)
+        t.update_description(options.update, ' '.join(options.args))
+    elif options.delete:
+        t.delete(options.delete)
     elif len(options.args) == 2:
-        copy_template(options.args[0], options.args[1])
-    elif len(options.args) == 1:
-        copy_template(options.args[0], os.path.join(os.getcwd(), options.args[0]))
+        t.copy_template(options.args[0], options.args[1], options.overwrite)
     else:
         parser.print_help()
-    
-    
-    
